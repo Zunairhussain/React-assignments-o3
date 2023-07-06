68ba8b0a8b90bf02dd83e08d8cb9073473b564d2
Zunairhussain <zunairhussain065@gmail.com>
2023-07-06 06:19:51 +0500

    assignment

---
+++ src/components/body/Body.js
@@ -0,0 +1,45 @@
+import "./Body.css"
+import imgbr from "./br.jpg"
+import imgsr from "./sr.jpg"
+
+function Body() {
+    return(
+        <div className="Body">
+            <img src={imgbr} alt="images not found"></img>
+            <h4>Cable Tray</h4>
+            <p>We have so many different kind and size of Cable Tray in M/S. Galvanize and coated sheet. You can purchase S.S Cable Tray from us too. Cable Tray of any industries you can buy from us.
+We have so many different kind and size of Cable Tray in M/S. Galvanize and coated sheet. You can purchase S.S Cable Tray from us too. Cable Tray of any industries you can buy from us.
+We have so many different kind and size of Cable Tray in M/S. Galvanize and coated sheet. You can purchase S.S Cable Tray from us too. Cable Tray of any industries you can buy from us.</p>
+<img src={imgsr} alt="images not found"></img>
+       <table >
+       <tr>
+                <td>
+                    <li>Ara</li>
+                    <li>profile</li>
+                    <li>sheet</li>
+                    <li>170*</li>
+                </td>
+                <td>
+                    <li>Ara</li>
+                    <li>profile</li>
+                    <li>sheet</li>
+                    <li>170*</li>
+                </td>
+                <td>
+                    <ol>
+                        <li>Ara</li>
+                        <li>profile</li>
+                        <li>sheet</li>
+                        <li>170*</li>
+                    </ol>
+                </td>
+                </tr>
+       </table>
+       
+       
+       
+       
+        </div>
+    )
+}
+export default Body;
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+
+function Header(){
+    return(
+        <div className="Header">
+             <h4>Ara Profile</h4>
+             <p>profile sheet 0.40 mm to 1.00 mm 28 gauge up to 20 gauge and worth 100 mm (39") X 1220 mm (48") 1500 mm (50*) and send where you f want. Gi Sheet and Roll, Coated Sheet and Roll, M/S Sheet and Roll, H.R.C Sheet and Roll, Galvanize Sheet and Roll and Stain-less steel also can profit by us</p>
+        </div>
+    )
+}
+export default Header;
